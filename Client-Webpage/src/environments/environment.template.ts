11240c0181758c2aae7f7c83d7e53b25ed427f07
Alexander Zang <mail@alexanderzang.de>
2024-02-09 23:39:08 +0100

    edited readme instructions, removed passwords

---
+++ Client-Webpage/src/environments/environment.template.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  MQTT_BROKER_NAME: '',
+  MQTT_BROKER_PORT: 1883,
+  MQTT_BROKER_IP: '',
+  MQTT_BROKER_WEB_SOCKET_PORT: 15676,
+  MQTT_BROKER_USER: '',
+  MQTT_BROKER_PASSWORD: ''
+};
+
